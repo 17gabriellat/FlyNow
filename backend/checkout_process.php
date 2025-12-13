6f0d19b97076d8defae9538a6ef933b383ef7c54
17gabriellat <17gabygabriella@gmail.com>
2025-12-13 12:27:32 +0700

    update environtment

--- backend/checkout_process.php
+++ backend/checkout_process.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 require_once "db.php"; // koneksi $conn
-require_once __DIR__ . '/env.php';
+require_once __DIR__ . '/../env.php';
 
 /* =============================================================
    1. MUST LOGIN
--- env.php
+++ env.php
@@ -8,7 +8,7 @@ if (!file_exists($envFile)) {
 $lines = file($envFile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 
 foreach ($lines as $line) {
-    if (str_starts_with(trim($line), '#')) continue;
+    if (strpos(trim($line), '#') === 0) continue;
 
     [$key, $value] = explode('=', $line, 2);
     putenv(trim($key) . '=' . trim($value));
